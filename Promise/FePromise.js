16c593fd3af0b6d6baba4b175106d4c94f284965
fengye <719130272@qq.com>
2018-11-27 10:51:28 +0800

    FePromise: 解决多个Resolve调用问题

--- Promise/FePromise.js
+++ Promise/FePromise.js
@@ -4,7 +4,10 @@ class FePromise {
     this.callback = undefined // 用来保存 then 传入的回调函数
     let _this = this
     // resolve reject all race FePromise的方法
+    this.isResolved = false
     let resolve = function(val) {
+      if(_this.isResolved) return
+      _this.isResolved = true
       _this.callback && _this.callback(val)
     }
     fn(resolve)
@@ -18,6 +21,8 @@ var p = new FePromise(function (resolve) {
   console.log(1)
   setTimeout(function () {
     resolve(2)
+    resolve(3)
+    resolve(4)
   }, 1000)
 })
 p.then(function (val) {
